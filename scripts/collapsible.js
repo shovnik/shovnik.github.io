9469dec5e85402821a5313e7665f9e081e45c5a2
Shovnik <shovnik.bhattacharya@gmail.com>
2018-03-19 01:45:46 -0400

    Added roles page and major styling

---
+++ scripts/collapsible.js
@@ -0,0 +1,12 @@
+const collapsible = document.getElementsByClassName("collapsible");
+for (let i = 0; i < collapsible.length; i++) {
+  collapsible[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const content = this.nextElementSibling;
+    if (content.style.display === "block"){
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
