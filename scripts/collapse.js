c73387e61d271e0d501c798e224b35784b19eb6d
Shovnik <shovnik.bhattacharya@gmail.com>
2018-03-22 22:36:22 -0400

    Few more allegiances and a lot of styling

--- scripts/collapse.js
+++ scripts/collapse.js
@@ -1,12 +1,18 @@
-const collapsible = document.getElementsByClassName("collapse");
-for (let i = 0; i < collapsible.length; i++) {
-  collapsible[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    const content = this.nextElementSibling;
-    if (content.style.display === "block"){
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+$( document ).ready(function() {
+  $('.collapse')
+    .map(function(index, element) {
+      $(element)
+        .parent()
+        .on("click", function() {
+          const collapse = $(this).children('.collapse');
+          const collapsible = $(this).children('.collapsible');
+          if ($(collapse).hasClass("active")) {
+            $(collapse).removeClass("active");
+            $(collapsible).css('display', "none")
+          } else {
+            $(collapse).addClass("active");
+            $(collapsible).css("display", "block")
+          }
+        })
+    });
+});
